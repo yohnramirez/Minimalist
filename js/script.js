ca1420bc9df95ac4a24d8cd62f0d8133e7b6f3b8
Sebastian Ramirez <yohnsilva999@hotmail.com>
2020-09-17 22:38:51 -0500

    Version 1

---
+++ js/script.js
@@ -0,0 +1,16 @@
+//FUNCIÓN BOTÓN TOP
+$(window).scroll(function(){
+  if($(this).scrollTop() > 300){ 
+    $("#js_up").slideDown(300); 
+  }else{ // si no
+    $("#js_up").slideUp(300);
+  }
+});
+
+$("#js_up i").on('click', function (e) { 
+  e.preventDefault(); 
+  $("body,html").animate({ 
+    scrollTop: 0 
+  },700); 
+  return false;
+});
